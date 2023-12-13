1dd6d0eb784e4a23b48bce251e7058b7ec22d2fc
XainFaith <XainFaith@gmail.com>
2023-12-12 22:35:43 -0800

    Imported Css styles and Added Game Field to test

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import Field from "./components/field.jsx";
 
 export default function App()
 {
-    return <div></div>;
+    return <div id="GameContainer">
+        <Field gridSize={5} bombCount={5}/>
+    </div>;
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 
 import App from "./app.jsx"
+import "./app.css";
 
 const container = document.getElementById('app');
 
