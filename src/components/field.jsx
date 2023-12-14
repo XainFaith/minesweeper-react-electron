1dc405ca0b21a90bb6f53475d54189d5d66b92ca
XainFaith <XainFaith@gmail.com>
2023-12-13 16:54:06 -0800

    extracted game field generation to own function

--- src/components/field.jsx
+++ src/components/field.jsx
@@ -1,140 +1,152 @@
 import React from "react";
 import Tile from "./tile.jsx";
 import "./field.css";
+import { useState } from "react";
 
 export default function Field({onGameStateChange, gridSize, bombCount})
 {
-    console.log("Constructing game field");
+    const [gameField, setGameField] = React.useState(null);
 
-    const gameFieldValues = [gridSize * gridSize];
-
-    //Generate random bomb placments
-    for(let bombs =0; bombs < bombCount;)
+    function GenerateGameField()
     {
-        let x = Math.floor(Math.random() * gridSize);
-        let y = Math.floor(Math.random() * gridSize);
-        let index = y * gridSize + x;
-        if(gameFieldValues[index] != -1)
-        {
-            gameFieldValues[index] = -1;
-            bombs++;
-        }
-    }
+        console.log("Generating Game Field");
+        let gameFieldValues = [gridSize * gridSize];
 
-    //Walk the Game Field and determine tile to bomb proximity values
-    for(let y=0; y < gridSize; y++)
-    {
-        for(let x=0; x < gridSize; x++)
+        //Generate random bomb placments
+        for(let bombs =0; bombs < bombCount;)
         {
+            let x = Math.floor(Math.random() * gridSize);
+            let y = Math.floor(Math.random() * gridSize);
             let index = y * gridSize + x;
             if(gameFieldValues[index] != -1)
             {
-                let tileValue = 0;
-                let utilIndex= 0;
-                //Check surrounding tiles
-                if(x-1 >= 0 && y-1 >= 0) //Top Left
-                {
-                    utilIndex = (y-1) * gridSize + (x-1);
-                    if(gameFieldValues[utilIndex] == -1)
-                    {
-                        tileValue++;
-                    }
-                }
-
-                if(y-1 >= 0) //Top middle
-                {
-                    utilIndex = (y-1) * gridSize + x;
-                    if(gameFieldValues[utilIndex] == -1)
-                    {
-                        tileValue++;
-                    }
-                }
-
-                if(x+1 < gridSize && y-1 >= 0) //Top Right
-                {
-                    utilIndex = (y-1) * gridSize + (x+1);
-                    if(gameFieldValues[utilIndex] == -1)
-                    {
-                        tileValue++;
-                    }
-                }
-
-                if(x-1 >= 0) //Left
-                {
-                    utilIndex = y * gridSize + (x-1);
-                    if(gameFieldValues[utilIndex] == -1)
-                    {
-                        tileValue++;
-                    }
-                }
-
-                if(x+1 < gridSize) //Right
-                {
-                    utilIndex = y * gridSize + (x+1);
-                    if(gameFieldValues[utilIndex] == -1)
-                    {
-                        tileValue++;
-                    }
-                }
-
-                if(x-1 >= 0 && y+1 < gridSize) //Bottom Left
-                {
-                    utilIndex = (y+1) * gridSize + (x-1);
-                    if(gameFieldValues[utilIndex] == -1)
-                    {
-                        tileValue++;
-                    }
-                }
-
-                if(y+1 < gridSize) //Bottom
-                {
-                    utilIndex = (y+1) * gridSize + x;
-                    if(gameFieldValues[utilIndex] == -1)
-                    {
-                        tileValue++;
-                    }
-                }
-
-                if(x+1 < gridSize && y+1 < gridSize) //Bottom Right
-                {
-                    utilIndex = (y+1) * gridSize + (x+1);
-                    if(gameFieldValues[utilIndex] == -1)
-                    {
-                        tileValue++;
-                    }
-                }
-
-                gameFieldValues[index] = tileValue;
+                gameFieldValues[index] = -1;
+                bombs++;
             }
         }
-    }
 
-    //Generate Game Tiles
-    const rows = [];
-    for(let y=0; y < gridSize; y++)    
-    {
-        const gameTiles = [];
-        for(let x =0; x < gridSize; x++)
+        //Walk the Game Field and determine tile to bomb proximity values
+        for(let y=0; y < gridSize; y++)
         {
-            let index = y * gridSize + x;
-            let isBombTile = false;
-
-            if(gameFieldValues[index] == -1)
+            for(let x=0; x < gridSize; x++)
             {
-                isBombTile = true;
-            }
+                let index = y * gridSize + x;
+                if(gameFieldValues[index] != -1)
+                {
+                    let tileValue = 0;
+                    let utilIndex= 0;
+                    //Check surrounding tiles
+                    if(x-1 >= 0 && y-1 >= 0) //Top Left
+                    {
+                        utilIndex = (y-1) * gridSize + (x-1);
+                        if(gameFieldValues[utilIndex] == -1)
+                        {
+                            tileValue++;
+                        }
+                    }
 
-            gameTiles.push(<Tile isBomb={isBombTile} isRevealed={false} key={index}/>);
+                    if(y-1 >= 0) //Top middle
+                    {
+                        utilIndex = (y-1) * gridSize + x;
+                        if(gameFieldValues[utilIndex] == -1)
+                        {
+                            tileValue++;
+                        }
+                    }
+
+                    if(x+1 < gridSize && y-1 >= 0) //Top Right
+                    {
+                        utilIndex = (y-1) * gridSize + (x+1);
+                        if(gameFieldValues[utilIndex] == -1)
+                        {
+                            tileValue++;
+                        }
+                    }
+
+                    if(x-1 >= 0) //Left
+                    {
+                        utilIndex = y * gridSize + (x-1);
+                        if(gameFieldValues[utilIndex] == -1)
+                        {
+                            tileValue++;
+                        }
+                    }
+
+                    if(x+1 < gridSize) //Right
+                    {
+                        utilIndex = y * gridSize + (x+1);
+                        if(gameFieldValues[utilIndex] == -1)
+                        {
+                            tileValue++;
+                        }
+                    }
+
+                    if(x-1 >= 0 && y+1 < gridSize) //Bottom Left
+                    {
+                        utilIndex = (y+1) * gridSize + (x-1);
+                        if(gameFieldValues[utilIndex] == -1)
+                        {
+                            tileValue++;
+                        }
+                    }
+
+                    if(y+1 < gridSize) //Bottom
+                    {
+                        utilIndex = (y+1) * gridSize + x;
+                        if(gameFieldValues[utilIndex] == -1)
+                        {
+                            tileValue++;
+                        }
+                    }
+
+                    if(x+1 < gridSize && y+1 < gridSize) //Bottom Right
+                    {
+                        utilIndex = (y+1) * gridSize + (x+1);
+                        if(gameFieldValues[utilIndex] == -1)
+                        {
+                            tileValue++;
+                        }
+                    }
+
+                    gameFieldValues[index] = tileValue;
+                }
+            }
         }
 
-        let rowKey = "row" + y;
-        rows.push(<div className="FieldRow" key={rowKey}>{gameTiles}</div>);
-        console.log("adding row");
+        //Generate Game Tiles
+        const rows = [];
+        for(let y=0; y < gridSize; y++)    
+        {
+            const gameTiles = [];
+            for(let x =0; x < gridSize; x++)
+            {
+                let index = y * gridSize + x;
+                let isBombTile = false;
+
+                if(gameFieldValues[index] == -1)
+                {
+                    isBombTile = true;
+                }
+
+                gameTiles.push(<Tile isBomb={isBombTile} isRevealed={true} key={index} bombProximity={gameFieldValues[index]}/>);
+            }
+
+            let rowKey = "row" + y;
+            rows.push(<div className="FieldRow" key={rowKey}>{gameTiles}</div>);
+        }
+
+        setGameField(rows);
     }
+
+    if(gameField === null)
+    {
+       GenerateGameField();
+    }
+   
 
     return (
         <div className="GameField">
-            {rows}
+            {gameField}
         </div>
     )
 
